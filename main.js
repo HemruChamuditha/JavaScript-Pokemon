c4266fefb7e15acfd4048ddb57b20e9325f39775
Hemru Chamuditha <chamudhitha123@gmail.com>
2024-07-13 21:37:05 +0530

    added pokemon to the screen

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 
+pokemonImg =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
+let container = document.querySelector(".container");
+
+for (let index = 1; index <= 151; index++) {
+  let pokemon = document.createElement("img");
+  pokemon.src = `${pokemonImg}${index}.png`;
+
+  container.append(pokemon);
+}
